7a2372ca2f9d29845da6aef2070245f3a0769c55
Mikhail Ignatenko <ignatenkomikhail239@gmail.com>
2025-08-04 07:42:58 +0300

    First stage: Second review

--- stats-service/stats-client/src/main/java/ru/practicum/statsclient/StatsClientConfig.java
+++ stats-service/stats-client/src/main/java/ru/practicum/statsclient/StatsClientConfig.java
@@ -1,11 +1,17 @@
 package ru.practicum.statsclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class StatsClientConfig {
+
+    @Value("${stats.server.url}")
+    private String serverUrl;
+
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
