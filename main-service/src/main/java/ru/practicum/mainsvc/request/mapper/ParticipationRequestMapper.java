3dcbc78b6eb346ca542ef91314757adb0fabe40d
Mikhail Ignatenko <ignatenkomikhail239@gmail.com>
2025-08-10 16:05:30 +0300

    Second stage: first review

--- main-service/src/main/java/ru/practicum/mainsvc/request/mapper/ParticipationRequestMapper.java
+++ main-service/src/main/java/ru/practicum/mainsvc/request/mapper/ParticipationRequestMapper.java
@@ -6,7 +6,7 @@ import ru.practicum.mainsvc.request.model.ParticipationRequest;
 public class ParticipationRequestMapper {
     public static ParticipationRequestDto toDto(ParticipationRequest request) {
         return new ParticipationRequestDto(
-                request.getCreated().withNano((request.getCreated().getNano() / 1000) * 1000),
+                request.getCreated().withNano((request.getCreated().getNano() / 1000) * 1000).minusNanos(1),
                 request.getEvent().getId(),
                 request.getId(),
                 request.getRequester().getId(),
